6bd8d85eee705cd4e91c93d683c3c157d4a8a2c4
dl <manyouzhe@126.com>
2022-07-07 14:39:12 +0800

    转成AndroidX

--- demo/src/main/java/com/dl/androidmvi/MainActivity.kt
+++ demo/src/main/java/com/dl/androidmvi/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.dl.androidmvi
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
